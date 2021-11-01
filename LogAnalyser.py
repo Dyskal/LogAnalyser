6c9471e11eae7d765a506357017a14580dabf7e7
Dyskal <yoshilexis@gmail.com>
2021-11-01 23:31:33 +0100

    update

--- LogAnalyser.py
+++ LogAnalyser.py
@@ -15,22 +15,20 @@ class User:
 
     def add_ip(self, ip: str) -> None:
         try:
-            ip_address: [IPv4Address | IPv6Address] = check_ip(ip)
+            ip_address: IPv4Address | IPv6Address = check_ip(ip)
             if ip_address not in self.ip_list:
                 self.ip_list.append(ip_address)
         except ValueError:
             print(f'{ip} does not appear to be an IPv4 or IPv6 address.')
 
     def __str__(self) -> str:
-        out: str = f'{self.username}    {self.uuid.__str__()}    ['
+        out = [f'{self.username:16}\t{self.uuid.__str__()}\t[']
         for i in range(len(self.ip_list)):
-            out += self.ip_list[i].__str__()
+            out.append(f'{self.ip_list[i].__str__():15}')
             if i is not len(self.ip_list) - 1:
-                out += '    '
-            else:
-                pass
-        out += ']'
-        return out
+                out.append('\t')
+        string = ''.join(out).strip() + ']'
+        return string
 
     def __eq__(self, other) -> bool:
         return isinstance(other, User) and self.uuid == other.uuid
@@ -38,37 +36,48 @@ class User:
     def __hash__(self) -> int:
         return hash(self.uuid.__str__())
 
+    def __lt__(self, other) -> bool:
+        return self.username.casefold() < other.username.casefold()
 
-def analyze(content: str) -> None:
+
+def analyze(content: str, users_list: list[User]) -> None:
     lines_iter: Iterator[str] = iter(content.splitlines())
     for line in lines_iter:
         if 'User Authenticator' in line:
             user: User = User(line.split(' ')[7], UUID(line.split(' ')[9]))
+
             if user not in users_list:
                 users_list.append(user)
-            ip_iter: Iterator[str] = copy(lines_iter)
 
             curr_user: User = users_list[users_list.index(user)]
+            ip_iter: Iterator[str] = copy(lines_iter)
+
             for ip_line in ip_iter:
                 if curr_user.username + '[' in ip_line:
+                    print(ip_line)
                     curr_user.add_ip(ip_line.split('/')[2].split(':')[0])
                     break
 
 
-if __name__ == '__main__':
-    dirs_list: list[str] = []
+def main() -> None:
+    folders: list[str] = []
     users_list: list[User] = []
     if len(argv) > 1:
-        dirs_list.extend(argv[1::])
+        folders.extend(argv[1:])
     else:
-        dirs_list.append('logs')
-    for dir in dirs_list:
-        for file in listdir(dir):
+        folders.append('logs')
+    for folder in folders:
+        for file in listdir(folder):
             if file.endswith('.gz'):
-                with gopen(f'{dir}\\{file}', 'rb') as gf:
-                    analyze(gf.read().decode())
+                with gopen(f'{folder}\\{file}', 'rb') as gf:
+                    analyze(gf.read().decode(), users_list)
             else:
-                with open(f'{dir}\\{file}', 'r') as f:
-                    analyze(f.read())
+                with open(f'{folder}\\{file}', 'r') as f:
+                    analyze(f.read(), users_list)
+    users_list.sort()
     for users in users_list:
         print(users)
+
+
+if __name__ == '__main__':
+    main()
